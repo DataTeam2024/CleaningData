fc97630d2bce08a7226873f55993ecac584420b5
danaxsax <dana.salinas.tareas@gmail.com>
2024-05-04 16:17:20 -0600

    feat: top 10

--- cleaning.py
+++ cleaning.py
@@ -1,6 +1,8 @@
 import csv
 import emoji
 import pandas as pd
+from collections import Counter
+
 
 def remove_emojis(text):
     return ''.join(c for c in text if c not in emoji.EMOJI_DATA)
@@ -21,5 +23,20 @@ df=df.drop(columns=['date', 'time'])
 df.to_csv('HeyBank.csv', index=False)
 df.head()
 
+def get_top_words(csv_file):
+    df = pd.read_csv(csv_file)
+    
+    text = ' '.join(df['tweet'])
+    
+    words = text.split()
+    
+    word_counts = Counter(word for word in words if len(word) > 4)
+    
+    top_20_words = word_counts.most_common(10)
+    
+    with open('top_20_words.csv', 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['Word', 'Frequency'])
+        writer.writerows(top_20_words)
 
-
+get_top_words('HeyBank.csv')
