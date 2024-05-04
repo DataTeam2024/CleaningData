7a2eb74e51bc9943bf8828bdde847a368160f411
danaxsax <dana.salinas.tareas@gmail.com>
2024-05-04 15:56:56 -0600

    feat: - column 1 & 2

--- cleaning.py
+++ cleaning.py
@@ -1,5 +1,6 @@
 import csv
 import emoji
+import pandas as pd
 
 def remove_emojis(text):
     return ''.join(c for c in text if c not in emoji.EMOJI_DATA)
@@ -14,3 +15,11 @@ with open('HeyBank.csv', 'r', newline='') as file:
 with open('HeyBank.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerows(rows)
+
+df = pd.read_csv('HeyBank.csv')
+df=df.drop(columns=['date', 'time'])
+df.to_csv('HeyBank.csv', index=False)
+df.head()
+
+
+
