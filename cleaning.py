3368b8e9c159b1e1c50f9438b6686d7b6a0d99a1
danaxsax <dana.salinas.tareas@gmail.com>
2024-05-04 12:49:31 -0600

    feat: deleting emojis

--- cleaning.py
+++ cleaning.py
@@ -1,8 +1,20 @@
 import csv
-rows = []
-with open('HeyBank.csv') as file:
-    content = file.readlines()
-header = content[:1]
-rows = content[1:]
-print(header)
-print(rows)
+import emoji
+
+def remove_emojis(text):
+    # Remove emojis from the text
+    return ''.join(c for c in text if c not in emoji.EMOJI_DATA)
+
+# Abre el archivo CSV para lectura y escritura
+with open('HeyBank.csv', 'r', newline='') as file:
+    reader = csv.reader(file)
+    rows = list(reader)
+
+    # Procesa todas las filas en la columna número 3
+    for row in rows:
+        row[2] = remove_emojis(row[2])
+
+# Escribe los cambios de nuevo en el archivo CSV
+with open('HeyBank.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(rows)
