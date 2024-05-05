6fbb8149231ff4ffcd36788acd8240f75b8e2cb8
danaxsax <dana.salinas.tareas@gmail.com>
2024-05-05 09:52:26 -0600

    feat: define functions

--- cleaning.py
+++ cleaning.py
@@ -1,42 +1,33 @@
 import csv
 import emoji
-import pandas as pd
 from collections import Counter
-
+import pandas as pd
 
 def remove_emojis(text):
     return ''.join(c for c in text if c not in emoji.EMOJI_DATA)
 
-with open('HeyBank.csv', 'r', newline='') as file:
+with open('src/heyBankOriginal.csv', 'r', newline='') as file:
     reader = csv.reader(file)
     rows = list(reader)
 
     for row in rows:
         row[2] = remove_emojis(row[2])
 
-with open('HeyBank.csv', 'w', newline='') as file:
+with open('src/HeyBankCleaned.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerows(rows)
 
-df = pd.read_csv('/HeyBank.csv')
-df=df.drop(columns=['date', 'time'])
-df.to_csv('/HeyBank.csv', index=False)
-df.head()
-
 def get_top_words(csv_file):
     df = pd.read_csv(csv_file)
-    
     text = ' '.join(df['tweet'])
-    
     words = text.split()
-    
     word_counts = Counter(word for word in words if len(word) > 4)
-    
     top_20_words = word_counts.most_common(10)
     
-    with open('top_20_words.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    with open('src/topWords.csv', 'w', newline='', encoding='utf-8') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(['Word', 'Frequency'])
         writer.writerows(top_20_words)
 
-get_top_words('HeyBank.csv')
+
+get_top_words('src/heyBankTweet.csv')
